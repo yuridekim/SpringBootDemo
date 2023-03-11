a61fc2b858411c9b9722ee366da0ef75474d251f
yuridekim <cerisehill@korea.ac.kr>
2023-03-12 02:11:47 +0900

    Add email taken

--- src/main/java/com/example/demo/student/StudentRepository.java
+++ src/main/java/com/example/demo/student/StudentRepository.java
@@ -1,9 +1,20 @@
 package com.example.demo.student;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface StudentRepository
         extends JpaRepository<Student, Long> {
+
+    // select * from student where email !=
+    @Query("SELECT s from Student s where s.email = ?1") //getting an error here;
+    // how did I solve it: Student should have been capitalized
+    // Runs well in psql cmd with lower cases but I think the query is not the sql itself but something unique
+    // to Spring and Student is taken from somewhere else
+    Optional<Student> findStudentByEmail(String email);
+
 }
